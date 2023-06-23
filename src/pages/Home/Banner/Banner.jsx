14a2a1bf4b805a1e5bb81a83486bdff3e40dc97a
coders-sohan <mofizulislam0216@gmail.com>
2023-06-23 11:30:31 +0600

    update

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -6,28 +6,30 @@ import heroImage from "../../../assets/home/bgImage.jpeg";
 
 const Banner = () => {
   return (
-    <div className="overflow-hidden relative bg-black">
-      <div className="z-0 h-[80vh] overflow-hidden">
-        {/* <video autoPlay loop muted className="absolute z-0">
+    <>
+      <div className="overflow-hidden relative bg-black">
+        <div className="z-0 h-[80vh] overflow-hidden">
+          {/* <video autoPlay loop muted className="absolute z-0">
           <source src={hero1} type="video/mp4" />
           Your browser does not support the video tag.
         </video> */}
-        <img
-          className="absolute z-0 lg:h-full h-auto w-full"
-          src={heroImage}
-          alt="heroImage"
-        />
-        <div className="overlay absolute top-0 left-0 w-[100%] h-[100%]"></div>
-        <div className="absolute sm:relative grid grid-cols-12 sm:grid-cols-2  items-center z-10 w-full h-full">
-          <div className="col-span-8 px-12">
-            <Slider />
-          </div>
-          <div className="bg-black col-span-4 p-10 rounded-l-3xl">
-            <Form />
+          <img
+            className="absolute z-0 lg:h-full h-auto w-full"
+            src={heroImage}
+            alt="heroImage"
+          />
+          <div className="overlay absolute top-0 left-0 w-[100%] h-[100%]"></div>
+          <div className="absolute sm:relative grid grid-cols-12 sm:grid-cols-2 items-center z-10 w-full h-full">
+            <div className="col-span-8 px-12">
+              <Slider />
+            </div>
+            <div className="bg-black col-span-4 p-10 rounded-l-3xl">
+              <Form />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
