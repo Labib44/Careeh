e2bb6bedee449203c42632551da0a92210d65029
coders-sohan <mofizulislam0216@gmail.com>
2023-06-23 11:26:26 +0600

    update hero

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -12,8 +12,12 @@ const Banner = () => {
           <source src={hero1} type="video/mp4" />
           Your browser does not support the video tag.
         </video> */}
-        <img className="absolute z-0 lg:h-full h-auto w-full" src={heroImage} alt="heroImage" />
-        <div className="overlay absolute top-0 left-0 w-[100%] sm:h-[50%] md:h-[85.5%] h-[100%]"></div>
+        <img
+          className="absolute z-0 lg:h-full h-auto w-full"
+          src={heroImage}
+          alt="heroImage"
+        />
+        <div className="overlay absolute top-0 left-0 w-[100%] h-[100%]"></div>
         <div className="absolute sm:relative grid grid-cols-12 sm:grid-cols-2  items-center z-10 w-full h-full">
           <div className="col-span-8 px-12">
             <Slider />
