7681142f950214ade44c2e42f54ce048670bcebb
coders-sohan <mofizulislam0216@gmail.com>
2023-06-24 17:48:06 +0600

    update

--- src/pages/Enterprise/EnterpriseBanner/EnterpriseBanner.jsx
+++ src/pages/Enterprise/EnterpriseBanner/EnterpriseBanner.jsx
@@ -1,12 +1,31 @@
 import "./EnterpriseBanner.css";
-import { FaCheckCircle, FaPhoneAlt } from "react-icons/fa";
-import flag from "../../../assets/enterprise/bangladesh-flag.png";
+// import { FaCheckCircle, FaPhoneAlt } from "react-icons/fa";
+// import flag from "../../../assets/enterprise/bangladesh-flag.png";
+import Form from "../../Home/Banner/Form";
 
 const EnterpriseBanner = () => {
   return (
-    <div className="overflow-hidden sm:min-h-screen">
+    <>
+    
+    <div
+      className={`bannerImg min-h-[80vh] md:min-h-[60vh] bg-cover bg-center bg-no-repeat flex justify-center items-center relative `}
+    >
+      <div className="grid sm:grid-cols-1 sm:text-center grid-cols-2 sm:gap-10 gap-10 justify-between z-10 sm:py-10 sm:container">
+        <div className="ml-14 flex items-center">
+          <h2 className="sm:text-4xl md:text-5xl lg:text-6xl text-[42.25px] font-bold text-white leading-snug">
+            Go the distance with tech-first <br /> <span className="text-primary">enterprise logistics</span>
+          </h2>
+        </div>
+        <div className="bg-black px-12 py-20 sm:rounded-xl rounded-l-2xl z-10 sm:ml-0 md:ml-10 lg:ml-14 xl:ml-10 ml-60">
+          <Form />
+        </div>
+      </div>
+      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-40"></div>
+    </div>
+
+    {/* <div className="overflow-hidden sm:min-h-screen ">
       <div className="bannerImg">
-        <div className=" container mx-auto py-20 sm:py-0 grid grid-cols-2  sm:grid-cols-1 gap-5">
+        <div className="container mx-auto py-20 sm:py-0 grid grid-cols-2  sm:grid-cols-1 gap-5">
           <div className="text-white">
             <h1 className="text-5xl md:text-3xl sm:text-2xl font-bold py-5 leading-snug">
               Go the distance with tech-first enterprise logistics
@@ -88,7 +107,8 @@ const EnterpriseBanner = () => {
           </div>
         </div>
       </div>
-    </div>
+    </div> */}
+    </>
   );
 };
 
